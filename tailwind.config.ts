6cb61f69f49b0342296bd5e19e950e69706108a1
dangorbey <diggerpro45@gmail.com>
2023-09-18 23:20:58 -0400

    fixed next css warnings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,10 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'baskerville': ['baskerville-display-pt', 'serif'], // Ensure fonts with spaces have " " surrounding it.
+      'artifex': ['artifex-cf', 'serif']
+    },
   },
   plugins: [],
 } satisfies Config;
